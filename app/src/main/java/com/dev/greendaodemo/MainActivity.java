9b23539d13fd412c579d86cb5dab6225f20eeefb
guoxinqi <guoxinqi94@outlook.com>
2017-02-27 09:13:38 +0800

    GreenDao 的基本使用

---
+++ app/src/main/java/com/dev/greendaodemo/MainActivity.java
@@ -0,0 +1,16 @@
+package com.dev.greendaodemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * greenDao3.0以后的版本更加的好用
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/dev/greendaodemo/bean/Shop.java
@@ -0,0 +1,11 @@
+package com.dev.greendaodemo.bean;
+
+/**
+ * 作者：Michael
+ * <p>
+ * 时间：2017/2/26 20:44
+ */
+
+public class Shop {
+
+}
